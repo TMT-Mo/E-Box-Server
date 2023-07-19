77d0596035e4f5faf61ce8f10a8804e83d85caa9
Mo <113235471+TMT-Mo@users.noreply.github.com>
2023-07-19 11:21:16 +0700

    content-type

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", CLIENT_HOST);
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, application/json", );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
